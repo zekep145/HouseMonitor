7936f3d4f948a8e8e05cf142deb5d818692887f2
Zeke Pendexter <zpendexter@gmail.com>
2017-06-18 00:20:02 +0000

    Initial file creation

---
+++ Camera.py
@@ -0,0 +1,13 @@
+# This class is used for controlling the camera
+
+
+class Camera:
+
+	def __init__(self):
+		print("Creating new Camera object")
+
+	def TakePicture(self):
+		print("Fill in implementation")
+
+	def TakeVideo(self, time):
+		print("Fill in implementation") 
---
+++ MotionSensor.py
@@ -0,0 +1,10 @@
+# This class contains all methods that interact with the motion sensor
+
+class MotionSensor:
+
+	def __init__(self):
+		print("Creating new MotionSensor Object")
+
+	def WaitForMotion(self):
+		print("Fill in implementation")
+
